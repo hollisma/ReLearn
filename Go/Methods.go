fbb4b697a9e9c0221ccc6616e601835c094a5c99
Hollis Ma <hollism@princeton.edu>
2019-07-20 16:38:43 -0400

    updated Methods.go

--- Go/Methods.go
+++ Go/Methods.go
@@ -12,13 +12,27 @@ type Vertex struct {
 
 // Receiver arg is (v Vertex)
 func (v Vertex) Abs() float64 {
-	return math.Sqrt(v.X*v.X + v.Y+v.Y)
+	return math.Sqrt(v.X*v.X + v.Y*v.Y)
 }
+// Can only have receivers on types defined in this package (file)
 
 // No receiver
 func Abs(v Vertex) float64 {
 	return math.Sqrt(v.X*v.X + v.Y*v.Y)
 }
+
+// If you want to modify receiver(struct) then use pointer receiver
+func (v *Vertex) Scale(f float64) {
+	v.X *= f
+	v.Y *= f
+}
+
+// Scale as regular function
+func Scale(v *Vertex, f float64) {
+	v.X *= f
+	v.Y *= f
+}
+// Advantage of pointer receiver is that you can modify value and so you don't need to include it with each method call
 
 func main() {
 	fmt.Println("----------Functional----------\n")
@@ -27,6 +41,13 @@ func main() {
 	fmt.Println(v.Abs())
 	fmt.Println("---------Regular func---------")
 	fmt.Println(Abs(v))
+	fmt.Println("-------Pointer receiver-------")
+	v.Scale(10)
+	fmt.Println(v.Abs())
+	(&v).Scale(2)  // Also works
+	fmt.Println(v.Abs())
+	Scale(&v, 5)  // Scale(v, 5) won't work
+	fmt.Println(v.Abs())
 }
 
 // This is a cheat sheet made from the Golang tour @ https://tour.golang.org
